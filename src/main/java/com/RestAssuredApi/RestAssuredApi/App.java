2c62d3b3fc7256db78044d0f6a9f7fef9aa81f3c
nafis <nafis.ahmad@badabusiness.com>
2024-02-06 15:25:45 +0530

    RestAssure api

---
+++ src/main/java/com/RestAssuredApi/RestAssuredApi/App.java
@@ -0,0 +1,13 @@
+package com.RestAssuredApi.RestAssuredApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
