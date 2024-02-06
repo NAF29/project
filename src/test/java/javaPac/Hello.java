de9ccb66dc412b9e0f12b8c399b46c8b5353c996
nafis <nafis.ahmad@badabusiness.com>
2024-02-06 14:25:40 +0530

    this is new changes

--- src/test/java/javaPac/Hello.java
+++ src/test/java/javaPac/Hello.java
@@ -2,9 +2,18 @@ package javaPac;
 
 public class Hello {
 
-	int a= 10;
-	int b=20;
-	int z=a+b;
 	
-	
+	public static void main(String args[]) {
+		int a= 10;
+		int b=20;
+		int z=a+b;
+		System.out.println();
+		
+		String Text = "Hello World";
+		StringBuilder sb = new StringBuilder(Text);
+		StringBuilder rev= sb.reverse();
+		System.out.println(rev);
+		
+		
+	}
 }
